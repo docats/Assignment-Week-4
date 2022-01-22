5df4d3a705dc6a51e267cfd30e42cff9f7b3fe6b
DESKTOP-OAUP1PE\User <j58701076@yahoo.com.tw>
2022-01-23 00:31:58 +0800

    作業3

--- python-flask/index.py
+++ python-flask/index.py
@@ -3,8 +3,8 @@ from distutils.log import debug
 from flask import Flask # 載入Flask 
 from flask import request # 載入Request物件(要取得POST參數值)
 from flask import redirect # 載入redirect函式
-from flask import render_template
-from markupsafe import re #使用樣板引擎
+from flask import render_template # 使用樣板引擎
+from flask import session # 使用session
 
 # 建立 Application物件，可以設定靜態檔案的路徑處理
 # 所有在 static 資料夾底下的檔案，都對應到網址路徑 /static/ 檔案名稱
@@ -14,36 +14,36 @@ app=Flask(
     static_url_path="/static" #靜態檔案對應的網址路徑
     ) 
 
+# 使用 Session密鑰
+app.secret_key = "any string but secret" 
+
 
 # 建立路徑 / 對應的處理函式
 @app.route('/')
 def index(): #用來回應路徑 / 的處理函式
-    # 網址的導向
-    # return redirect("/en/") # 導向到路徑 /en/
-
+    
     return render_template('index.html') # 回傳網站首頁內容
 
 #建立路徑 /sigin對應的處理函式
 @app.route("/sigin",methods=['POST'])
 def sigin():
-    # username = request.values['uname']
-    # password = request.values['psw']
     username = request.form['uname']
     password = request.form['psw']
-    print("長度",len(username))
     if username == "test" and password == "test":
-        return redirect('/member')
+        session["username"]=username
+        return redirect('/member/?login=已登入')
     elif len(username)==0 or len(password)==0:
         return redirect('/empty/?message=帳號或密碼不能為空')
     else:
-        # input=request.args.get("uname","")
         return redirect('/error/?message=帳號或密碼錯誤')
 
 # 建立路徑 /member對應的處理函式
-@app.route("/member")
-def member():
-    
-    return  render_template('member.html')
+@app.route("/member/",methods=['GET'])
+def member():    
+    login=request.args.get("login","")
+    name=session["username"]
+    print("使用者名稱:",name)
+    return  render_template('member.html',login=login)
 
 # 建立路徑 /error對應的處理函式
 @app.route("/error/",methods=['GET'])
@@ -55,8 +55,12 @@ def error():
 @app.route("/empty/",methods=['GET'])
 def empty():
     message=request.args.get("message")
-    return render_template('empty.html')
+    return render_template('empty.html',message=message)
 
+#建立路徑 /signout對應的處理函式
+@app.route("/signout",methods=['GET'])
+def signout():
+    return redirect("/")
 
 # 動態路由:建立路徑 /user/使用者名稱，對應的處理函式
 # @app.route("/user/<username>")
